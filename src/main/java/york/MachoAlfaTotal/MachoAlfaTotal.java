8ff28fc0281660568b42f029e433452a43dcb450
unknown <pablosg003@gmail.com>
2021-07-14 21:05:14 +0200

    public

--- src/main/java/york/MachoAlfaTotal/MachoAlfaTotal.java
+++ src/main/java/york/MachoAlfaTotal/MachoAlfaTotal.java
@@ -10,9 +10,6 @@ import net.dv8tion.jda.api.entities.Activity.ActivityType;
 
 public class MachoAlfaTotal {
 	
-	public static final String GENERAL_TXT = "864487370182492200";
-	public static final String GENERAL_VOZ = "864141288420016162";
-	
 	public static final String prefijo ="/";
 	public static boolean usarPrefijo = false;
 	public static boolean gritosAleatorios = true;
@@ -20,7 +17,7 @@ public class MachoAlfaTotal {
 	
 	public static void main(String[] args) throws LoginException{
 		
-		JDA jda = JDABuilder.createDefault("ODY0MTQyODY3NDY3MjA2NjY2.YOxJwg.HcnH181sq4iZvD8Ren4YGK0_Mhs").build();
+		JDA jda = JDABuilder.createDefault("your token here").build();
 		
 		jda.getPresence().setStatus(OnlineStatus.ONLINE);
 		jda.getPresence().setActivity(Activity.of(ActivityType.WATCHING, "Macho alfa total", "https://youtu.be/6peMik4tpLA"));
